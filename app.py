8a657b069b5dae8d75cbdac6e5964795b4f871b6
fuyingdi <fuyingdi@126.com>
2018-05-20 19:25:43 +0800

    修改表单部分

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from form import MyloveForm
 from database import db
 import pymongo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'FOR MY DEAREST GIRL'
 
 
-@app.route('/love')
+@app.route('/love', methods=['POST', 'GET'])
 def love():
     form = MyloveForm()
     if form.validate_on_submit():
@@ -14,22 +15,20 @@ def love():
         lovername = form.lovername.data
         db.lovers.insert_one({'name': myname})
         db.beloved.insert_one({'name': lovername})
-    return render_template('submit.html',form=form)
+    return redirect(url_for('home'))
+
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    return render_template('index.html')
 
 
 def judge(name):
-    cur=db.beloved.find({'name':name})
+    cur = db.beloved.find({'name': name})
     if cur:
         return True
     else:
         return False
-
-
-@app.route('/home')
-def index():
-    pass
-
-
 
 
 if __name__ == '__main__':
